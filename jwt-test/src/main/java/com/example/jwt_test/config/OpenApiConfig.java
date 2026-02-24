bb8037eb4278c0ca8e94c304a7fb13b334d6b259
kickbell <dev.kickbell@gmail.com>
2026-02-24 17:12:16 +0900

    feat: application.yml으로 포트번호, 시크릿키 등 관리하도록 변경

--- jwt-test/src/main/java/com/example/jwt_test/config/OpenApiConfig.java
+++ jwt-test/src/main/java/com/example/jwt_test/config/OpenApiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +13,12 @@ import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
+
+    // application.yml(또는 properties)에 설정된 server.port 값을 가져옵니다.
+    // 설정이 없으면 기본값으로 8080을 사용합니다
+    @Value("${server.port:8080}")
+    private String port;
+
     @Bean
     public OpenAPI openAPI() {
         SecurityScheme securityScheme = new SecurityScheme()
@@ -25,7 +32,7 @@ public class OpenApiConfig {
                         .version("v1.0.0")
                 )
                 .servers(List.of(
-                        new Server().url("http://localhost:10000")
+                        new Server().url("http://localhost:" + port)
                                 .description("개발용 서버 주소")
                 ))
                 .components(
